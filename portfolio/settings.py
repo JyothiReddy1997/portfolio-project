71d960378a331bd8597f0a40dddbc34694748f8b
Jyothi Reddy Bhavanam <31843963+JyothiReddy1997@users.noreply.github.com>
2020-04-10 12:47:32 -0700

    Update settings.py
    added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -134,3 +134,8 @@ STATIC_URL = '/static/'         #eggs
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"                       #can be anything "/eggs/"
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
